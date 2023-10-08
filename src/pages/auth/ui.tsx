ded3ef8b9196d92393a9ff7b7740dcd6e832ec75
White11010 <beliavski26@gmail.com>
2023-10-08 14:16:01 +0300

    fixed auth page display

--- src/pages/auth/ui.tsx
+++ src/pages/auth/ui.tsx
@@ -1,7 +1,8 @@
 import { AuthForms } from '~widgets/auth-forms';
+import classes from './ui.module.scss';
 
 export function AuthPage() {
-  return <section>
+  return <section className={classes.auth}>
     <AuthForms />
   </section>;
 }
